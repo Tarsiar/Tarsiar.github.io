ec33aa656fd5aa872f2f41bfc901e824381e14d2
dylan <dylan@webpowerchina.com>
2023-05-22 13:01:41 +0800

    ppp-

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,9 +1,17 @@
+/*
+ * @Author: dylan dylan@webpowerchina.com
+ * @Date: 2023-05-22 11:19:22
+ * @LastEditors: dylan dylan@webpowerchina.com
+ * @LastEditTime: 2023-05-22 12:59:15
+ * @FilePath: \Tarsiar.github.io\.vitepress\config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 // https://vitepress.dev/reference/default-theme-config
 export default defineConfig({
-  base: '/',
+  base: '',
   title: "mc 小唐",
   description: "知心",
   lastUpdated: true,
